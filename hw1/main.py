03004fb34a79fcec60de9e9ca3ede1845025d211
Kevin <b03902086@ntu.edu.tw>
2016-11-05 14:31:20 +0800

    replace -> [:]

--- hw1/main.py
+++ hw1/main.py
@@ -24,18 +24,17 @@ while True:
     if 'PRIVMSG' in line:
         nickname = line[1 : line.find('!~')]
         msg = line.split(':')[-1]
-        if msg == '@help': # more text accepted?
+        if msg == '@help':
             send_('PRIVMSG #CN_Demo :@repeat <String> (%s)' % nickname, cli_socket)
             send_('PRIVMSG #CN_Demo :@cal <Expression> (%s)' % nickname, cli_socket)
             send_('PRIVMSG #CN_Demo :@play <Robot Name> (%s)' % nickname, cli_socket)
             send_('PRIVMSG #CN_Demo :@guess <Integer> (%s)' % nickname, cli_socket)
             send_('PRIVMSG #CN_Demo :@youtube <Song> (%s)' % nickname, cli_socket)
         elif msg.startswith('@repeat '): # no word after @repeat accepted?
-            # this replace all @repeat, which is not good
-            msg = msg.replace('@repeat ', '')
+            msg = msg[8:]
             send_('PRIVMSG #CN_Demo :%s (%s)' % (msg, nickname), cli_socket)
-        elif msg.startswith('@cal'):
-            msg = msg.replace('@cal', '').replace('^', '**')
+        elif msg.startswith('@cal '):
+            msg = msg[5:].replace('^', '**')
             try:
                 ans = eval(msg)
                 send_('PRIVMSG #CN_Demo :%f (%s)' % (ans, nickname), cli_socket)
