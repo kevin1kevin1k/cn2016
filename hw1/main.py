923e83d5e827d83d9192d0f4e2b2c85bc4e90309
Kevin <b03902086@ntu.edu.tw>
2016-11-05 18:02:57 +0800

    refactor

--- hw1/main.py
+++ hw1/main.py
@@ -2,44 +2,47 @@ import socket
 import urllib
 from bs4 import BeautifulSoup
 
-def send_(string, skt):
-    skt.send(bytes(string + ' \r\n'))
-
 cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cli_socket.connect( ('irc.freenode.net', 6667) )
 cli_socket.send(bytes('NICK helloooooo \r\n'))
 cli_socket.send(bytes('USER beforeAT 8 * :realname \r\n'))
 cli_socket.send(bytes('JOIN #CN_Demo ILoveTA \r\n'))
 
+def send_(msg):
+    cli_socket.send(bytes('PRIVMSG #CN_Demo :%s \r\n' % msg))
+
 while True:
-    line = cli_socket.recv(1024).rstrip()
+    line = cli_socket.recv(1024)
+    if line[:4] == 'PING':
+        send(bytes(line.replace('PING', 'PONG')), cli_socket)
+        continue
+    
+    line = line.rstrip()
     if len(line) < 2:
         print 'Error: len(line) < 2, line:', line
         break
+
     print line
-    # cli_socket.send_(bytes('PRIVMSG #CN_Demo ' + 'hello ' + '\r\n'))
-    if line[:4] == 'PING':
-        send_(line.replace('PING', 'PONG'), cli_socket)
     
     if 'PRIVMSG' in line:
         nickname = line[1 : line.find('!~')]
         msg = line.split(':')[-1]
         if msg == '@help':
-            send_('PRIVMSG #CN_Demo :@repeat <String> (%s)' % nickname, cli_socket)
-            send_('PRIVMSG #CN_Demo :@cal <Expression> (%s)' % nickname, cli_socket)
-            send_('PRIVMSG #CN_Demo :@play <Robot Name> (%s)' % nickname, cli_socket)
-            send_('PRIVMSG #CN_Demo :@guess <Integer> (%s)' % nickname, cli_socket)
-            send_('PRIVMSG #CN_Demo :@youtube <Song> (%s)' % nickname, cli_socket)
+            send_('@repeat <String> (%s)' % nickname)
+            send_('@cal <Expression> (%s)' % nickname)
+            send_('@play <Robot Name> (%s)' % nickname)
+            send_('@guess <Integer> (%s)' % nickname)
+            send_('@youtube <Song> (%s)' % nickname)
         elif msg.startswith('@repeat '): # no word after @repeat accepted?
             msg = msg[8:]
-            send_('PRIVMSG #CN_Demo :%s (%s)' % (msg, nickname), cli_socket)
+            send_('%s (%s)' % (msg, nickname))
         elif msg.startswith('@cal '):
             msg = msg[5:].replace('^', '**')
             try:
                 ans = eval(msg)
-                send_('PRIVMSG #CN_Demo :%f (%s)' % (ans, nickname), cli_socket)
+                send_('%f (%s)' % (ans, nickname))
             except:
-                send_('PRIVMSG #CN_Demo :Format Error (%s)' % nickname, cli_socket)
+                send_('Format Error (%s)' % nickname)
         
     
 cli_socket.close()
