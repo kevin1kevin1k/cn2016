9c589bf1c045e0c288dc0320c3b3889eb0128444
Kevin <b03902086@ntu.edu.tw>
2017-01-07 20:46:24 +0800

    fix more addr bugs

--- hw2/UDP.h
+++ hw2/UDP.h
@@ -17,16 +17,16 @@ void create_socket(int *fd, struct sockaddr_in *addr, int port) {
     addr->sin_family = AF_INET;
     addr->sin_port = htons(port);
     addr->sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(*fd, (struct sockaddr *)addr, sizeof(addr));
+    bind(*fd, (struct sockaddr *)addr, sizeof(*addr));
 }
 
 void my_send(int fd, void *obj, struct sockaddr_in *addr) {
-    socklen_t len = sizeof(addr);
+    socklen_t len = sizeof(*addr);
     sendto(fd, obj, sizeof(obj), 0, (struct sockaddr *)addr, len);
 }
 
 
 void my_recv(int fd, void *obj, struct sockaddr_in *addr) {
-    socklen_t len = sizeof(addr);
+    socklen_t len = sizeof(*addr);
     recvfrom(fd, obj, sizeof(obj), 0, (struct sockaddr *)addr, &len);
 }
