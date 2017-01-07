dd6add936d46e3868dcad3ad79fcbb5ed0c75bd5
Kevin <b03902086@ntu.edu.tw>
2017-01-07 20:33:55 +0800

    fix addr bug

--- hw2/UDP.h
+++ hw2/UDP.h
@@ -13,7 +13,7 @@ void set_addr(struct sockaddr_in *addr, char *ip, int port) {
 
 void create_socket(int *fd, struct sockaddr_in *addr, int port) {
     *fd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&addr, 0, sizeof(addr));
+    memset(addr, 0, sizeof(*addr));
     addr->sin_family = AF_INET;
     addr->sin_port = htons(port);
     addr->sin_addr.s_addr = htonl(INADDR_ANY);
